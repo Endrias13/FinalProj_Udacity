7cbaa2dfcf694e46d6312030602e328cd7ede442
Endrias13 <33791240+Endrias13@users.noreply.github.com>
2024-06-15 16:34:31 -0400

    Update main.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -36,7 +36,7 @@ module "network" {
   virtual_network_name = "${var.virtual_network_name}"
   application_type     = "${var.application_type}"
   resource_type        = "NET"
-  resource_group       = "${var.resource_group}"
+  resource_group       = "${var.resource_name}"
   address_prefix_test  = "${var.address_prefix_test}"
 }
 
