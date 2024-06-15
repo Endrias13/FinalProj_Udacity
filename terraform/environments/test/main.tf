0e6099580247927cbf62acdd2a7fc34391617607
Endrias13 <33791240+Endrias13@users.noreply.github.com>
2024-06-15 15:28:41 -0400

    Try Again

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -19,7 +19,7 @@ terraform {
     storage_account_name  = var.storage_account_name
     container_name       = var.container_name
     key                   = var.key
-    access_key           = ""
+    access_key           = var.store_key
   }
 }
 
