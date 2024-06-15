2a88264e53a7494241db31650d7998f80dbc086e
Endrias13 <33791240+Endrias13@users.noreply.github.com>
2024-06-15 15:24:54 -0400

    Update main.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -18,7 +18,7 @@ terraform {
     resource_group_name   = var.resource_group_name
     storage_account_name  = var.storage_account_name
     container_name       = var.container_name
-    key                   = ""
+    key                   = var.key
     access_key           = ""
   }
 }
